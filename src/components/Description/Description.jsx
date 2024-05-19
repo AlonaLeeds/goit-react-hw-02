a67b697950aebe7697d1a76fd744d6c7a3999994
HelenYakovenko <elenus.light@gmail.com>
2024-05-19 18:51:03 +0300

    css styles

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -1,3 +1,5 @@
+
+
 export default function Description() {
    return (
       <div>
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -4,7 +4,6 @@ import css from "./Options.module.css"
 export default function Options ({ onFeedback, totalFeedback, onReset }) {
   return (
     <div className={css.button}>
-      <h2>Give Feedback</h2>
       <button onClick={() => onFeedback('good')}>Good</button>
       <button onClick={() => onFeedback('neutral')}>Neutral</button>
       <button onClick={() => onFeedback('bad')}>Bad</button>
